78db1cee01aa64c52e4ba0950bc3672b079fd59e
Isnu Nasrudin <isnunas@gmail.com>
2023-07-31 18:19:01 +0700

    403 fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function messages()
     {
         return $this->hasMany(Message::class);
+    }
+
+    public function canAccessFilament(): bool
+    {
+        return true;
     }
 }
